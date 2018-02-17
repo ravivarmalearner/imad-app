ab56c3bbb0b0e9f8bbda3e83ceb3f7a85c56a93c
ravivarmalearner <36413602+ravivarmalearner@users.noreply.github.com>
2018-02-17 23:06:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var articleOne={title:'Article one |Ravi varma',
+var articles={
+ 'article-One':{
+    title:'Article one |Ravi varma',
     heading:'Article one',
     date:'Feb 14 2018',
     content:`<p> this is article one</p>
              <p>I am creating article one</p>`
+},
+ 'article-Two':{
+    title:'Article Two |Ravi varma',
+    heading:'Article Two',
+    date:'Feb 17 2018',
+    content:`<p> this is article Two</p>
+             <p>I am creating article Two</p>`
+},
+ 'article-Three':{
+    title:'Article Three |Ravi varma',
+    heading:'Article Three',
+    date:'Feb 17 2018',
+    content:`<p> this is article Three</p>
+             <p>I am creating article Three</p>`
+},
 };
+
 function createtemplate(data){
 var title=data.title;
 var heading=data.heading;
@@ -48,24 +66,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:articleName',function(req,res){
+   var articleName = req.params.articleName; 
+    res.send(Createtemplate(articles[articleName]))
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/article-one',function(req,res){
-    res.send(createtemplate(articleOne));
-});
-
-app.get('/article-two',function(req,res){
-    res.send('Article two is reqested and served here');
-});
- 
- app.get('/article-three',function(req,res){
-    res.send('Article three is reqested and served here');
 });
 
 
