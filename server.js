241b6d22659e5105d8f5477f8bbf660dba0f1647
ravivarmalearner <36413602+ravivarmalearner@users.noreply.github.com>
2018-02-28 19:58:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'bmravivarmapatchamatla',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:db-bmravivarmapatchamatla-10199
+    password:process.env.DB_PASSWORD
 };
 var articles={
  'article-one':{
