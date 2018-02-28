a4e5c0d77fa38effcb7196d448e7d3c9fc97182e
ravivarmalearner <36413602+ravivarmalearner@users.noreply.github.com>
2018-02-28 19:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,17 +102,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/test-db',function(req,res){
-    pool.query('select * from test',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else{
-        res.send (JSON.stringify(result.rows));   
-        }
-    });
-    
-});
+
 
 
 
