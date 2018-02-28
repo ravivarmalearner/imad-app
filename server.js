2710022abb0d2d6765c7fe0415152dadc91ddf1d
ravivarmalearner <36413602+ravivarmalearner@users.noreply.github.com>
2018-02-28 19:49:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config);
 var counter=0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
