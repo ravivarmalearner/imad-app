d5525e341f442844b31cac8bcb0d4f2055e9724a
ravivarmalearner <36413602+ravivarmalearner@users.noreply.github.com>
2018-02-28 19:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
+var config={
+    user:'bmravivarmapatchamatla',
+    database:'bmravivarmapatchamatla',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var articles={
  'article-one':{
     title:'Article one |Ravi varma',
@@ -64,6 +72,19 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('select * from test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+        res.send (JSON.stringify(result));   
+        }
+    });
+    
 });
 var counter=0;
 app.get('/counter', function (req, res) {
